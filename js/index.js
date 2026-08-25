8ef2daadf31d20459104fe001b32dbfe28307c94
Elijah <177173296+Elijahb620@users.noreply.github.com>
2026-08-24 21:18:50 -0500

    added message form

--- js/index.js
+++ js/index.js
@@ -18,3 +18,51 @@ for (let i = 0; i<skills.length; i++){
     skill.textContent = skills[i];
     skillsList.appendChild(skill);
 }
+
+let messageForm = document.forms["leave_message"]
+
+messageForm.addEventListener("submit", function (event) {
+    event.preventDefault()
+
+
+    console.log(event)
+    let name = event.target.usersName.value
+    let email = event.target.usersEmail.value
+    let message = event.target.usersMessage.value
+
+    console.log(name, email, message);
+
+
+    let messageSection = document.querySelector("#messages")
+    let messageList = messageSection.querySelector("ul")
+    let newMessage = document.createElement("li")
+    newMessage.innerHTML = 
+        `<a href="mailto:${email}">${name}</a> 
+        <span>${message}</span>`;
+
+    messageList.appendChild(newMessage);
+
+    let removeButton = document.createElement("button")
+    removeButton.innerText = "remove"
+    removeButton.type = "button"
+
+    removeButton.addEventListener("click", function (event) {
+        let entry = removeButton.parentNode;
+        entry.remove();
+    })
+
+    newMessage.appendChild(removeButton);
+    
+    let h2 = document.querySelector("#messages h2")
+        h2.style.display = "inline"
+
+    messageForm.reset()
+    
+})
+
+if (!document.querySelector("#messages ul li")) {
+    document.querySelector("#messages ul").innerHTML = ""
+    let h2 = document.querySelector("#messages h2")
+    h2.style.display = "none"
+}
+
